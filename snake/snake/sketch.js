a148fc48f11c594f63642a356b24d7b8849a279c
toferj <toferj@gmail.com>
2017-05-30 17:42:24 -0500

    Added two new user options
    New options are 1) start in random location, and 2) start out moving in a random direction.

---
+++ snake/snake/sketch.js
@@ -0,0 +1,104 @@
+// Snake Game
+// Chris Jordan (toferj@gmail.com)
+// inspired by Dan Shiffman (http://codingtra.in)
+// Coding Challenge #3: The Snake Game (https://youtu.be/AaGK-fj-BAM)
+//
+// TODO:
+// - Add Count Down start (3, 2, 1...GO!)
+// - Add longest snake record... without a DB or some sort of online storage, this would only be per game session...
+// - It seems impossible to ride along the walls now. Check wall collision detection.
+//   (as a stopgap, I just made it where food will never be placed exactly on any wall)
+//
+// Other possible user controlled settings:
+//  - start snake at a random place on the screen
+//  - start snake moving in a random direction
+// Anything else?
+
+
+
+var snake, food;
+var scaleFactor = 20;
+var bg = {
+    r: 43,
+    g: 43,
+    b: 43
+};
+var settings = {};
+
+function setup() {
+    var canvas = createCanvas(600, 700);
+    canvas.parent('gameBoard');
+
+    var playAgain = select('#playAgain');
+    playAgain.mouseClicked(startOver);
+
+    // read settings
+    readSettings();
+    frameRate(settings.snakeSpeed);
+
+    // add our snake and our first piece of food...
+    snake = new Snake();
+    food = new Food();
+}
+
+function readSettings(){
+    settings = {};
+    settings.junkFoodFrequency = select('#dropFrequency').value();
+    settings.snakeSpeed = select('#snakeSpeed').value();
+    settings.growSpeed = select('#growSpeed').checked();
+    settings.randomStartPos = select('#randomStartPos').checked();
+    settings.randomStartVel = select('#randomStartVel').checked();
+    settings.debuggingOn = select('#debug').checked();
+    console.log(settings);
+}
+
+function keyPressed() {
+    switch (keyCode) {
+        case UP_ARROW:
+            if (snake.yspeed !== 1) {
+                snake.move(0, -1);
+            }
+            break;
+        case DOWN_ARROW:
+            if (snake.yspeed !== -1) {
+                snake.move(0, 1);
+            }
+            break;
+        case LEFT_ARROW:
+            if (snake.xspeed !== 1) {
+                snake.move(-1, 0);
+            }
+            break;
+        case RIGHT_ARROW:
+            if (snake.xspeed !== -1) {
+                snake.move(1, 0);
+            }
+            break;
+        default:
+
+    }
+}
+
+function draw() {
+    background(bg.r, bg.g, bg.b);
+    if (snake.eat()) {
+        food = new Food();
+    }
+    snake.update();
+    snake.show();
+    food.show();
+    if (snake.dead()) {
+        noLoop();
+    }
+}
+
+function startOver() {
+    // re-read settings in case any have changed...
+    readSettings();
+    frameRate(settings.snakeSpeed);
+
+    snake = new Snake();
+    food = new Food();
+    loop();
+
+}
---
+++ snake/snake/snake.js
@@ -0,0 +1,114 @@
+function Snake() {
+    var cols = floor(width / scaleFactor);
+    var rows = floor(height / scaleFactor);
+    if (settings.randomStartPos) {
+        this.pos = createVector(floor(random(scaleFactor, cols)), floor(random(scaleFactor, rows)));
+    } else {
+        this.pos = createVector(floor(cols / 2), floor(rows / 2));
+    }
+    this.pos.mult(scaleFactor);
+    if (settings.randomStartVel) {
+        if (random(1) < 0.5) {
+            this.xspeed = 1;
+            this.yspeed = 0;
+        } else {
+            this.xspeed = 0;
+            this.yspeed = 1;
+        }
+    } else {
+        this.xspeed = 0;
+        this.yspeed = 0;
+    }
+
+    this.totalEaten = 0;
+    this.tail = [];
+    this.isDead = false;
+    this.snakeSpeed = settings.snakeSpeed;
+    this.justAteJunk = false;
+};
+
+Snake.prototype.move = function(x, y) {
+    this.xspeed = x;
+    this.yspeed = y;
+};
+
+Snake.prototype.eat = function() {
+    // check to see if we've eaten food or not...
+    var d = dist(this.pos.x, this.pos.y, food.pos.x, food.pos.y);
+    if (d < 1) {
+        if (food.junkFood) {
+            this.totalEaten--;
+            this.tail.splice(0, 1);
+        } else {
+            this.totalEaten++;
+            if (settings.growSpeed) {
+                this.snakeSpeed += 0.5;
+                this.snakeSpeed = constrain(this.snakeSpeed, 5, 30);
+                // update the frameRate to inscrease the snake's speed.
+                frameRate(this.snakeSpeed);
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
+};
+
+Snake.prototype.dead = function() {
+    var d;
+    // check for wall collision...
+    if (this.pos.x < scaleFactor || this.pos.x > width - (scaleFactor * 2) || this.pos.y < scaleFactor || this.pos.y > height - (scaleFactor * 2)) {
+        this.isDead = true;
+        // let's color the head of the snake red indicating our collision visually...
+        fill(255, 0, 0);
+        rect(this.pos.x, this.pos.y, scaleFactor, scaleFactor);
+    }
+    // if we haven't detected a collision with any of the four walls... then check for collisions with our own tail...
+    if (!this.isDead) {
+        for (var i = 0; i < this.tail.length; i++) {
+            // check for self-collision...
+            d = dist(this.pos.x, this.pos.y, this.tail[i].x, this.tail[i].y);
+            if (d < scaleFactor) {
+                this.isDead = true;
+                // let's color the head of the snake red indicating our collision visually...
+                fill(255, 0, 0);
+                rect(this.pos.x, this.pos.y, scaleFactor, scaleFactor);
+                // we found a collision! no need to keep checking.
+                // break out and return collision.
+                break;
+            }
+        }
+    }
+    return this.isDead;
+};
+
+Snake.prototype.update = function() {
+    for (var i = 0; i < this.tail.length - 1; i++) {
+        this.tail[i] = this.tail[i + 1];
+    }
+    if (this.totalEaten >= 1) {
+        this.tail[this.totalEaten - 1] = createVector(this.pos.x, this.pos.y);
+    }
+
+    this.pos.x += this.xspeed * scaleFactor;
+    this.pos.y += this.yspeed * scaleFactor;
+
+    this.pos.x = constrain(this.pos.x, 0, width - scaleFactor);
+    this.pos.y = constrain(this.pos.y, 0, height - scaleFactor);
+};
+
+Snake.prototype.show = function() {
+    fill(255);
+    for (var i = this.tail.length - 1; i >= 0; i--) {
+        rect(this.tail[i].x, this.tail[i].y, scaleFactor, scaleFactor);
+        if (settings.debuggingOn) {
+            push();
+            fill(0);
+            textSize(14);
+            textAlign(CENTER, CENTER);
+            text(i, this.tail[i].x, this.tail[i].y, scaleFactor, scaleFactor);
+            pop();
+        }
+    }
+    rect(this.pos.x, this.pos.y, scaleFactor, scaleFactor);
+};
